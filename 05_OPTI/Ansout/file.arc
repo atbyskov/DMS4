a36ab7f1a2c3b558d08efd7fbee4daabdc5d0767
Zeetox12345 <christiandam77@gmail.com>
2026-04-06 15:37:52 +0200

    Optimization_Works!_ Lambda functions implemented, constraints implemented. We need to work further on more constraints, and optimization for speed.

--- 05_OPTI/Ansout/file.arc
+++ 05_OPTI/Ansout/file.arc
@@ -1,2 +1,2 @@
   ldstep   isubst           timval      curr timinc      next timinc           gammak      gammak_idof        gamma0kp1           normtk        normt0kp1           damage  ieqitr   ibsec 
-       1        1     1.000000e+00     1.000000e+00     0.000000e+00     0.000000e+00     0.000000e+00     0.000000e+00     1.733476e+03     0.000000e+00     0.000000e+00       6       0
+       1        1     1.000000e+00     1.000000e+00     0.000000e+00     0.000000e+00     0.000000e+00     0.000000e+00     2.215989e+03     0.000000e+00     0.000000e+00       9       0
--- 05_OPTI/optimization.py
+++ 05_OPTI/optimization.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy import optimize as spo
 from MyAPDLCall import RunAPDL
 from opt_logger import OptimizationLogger
-#from Post_Process import Util_LB, Util_NF, Util_S, Util_T, Util_BNS, Util_BR, Util_IN 
 from Post_Process import PostProcessor
 
 
@@ -10,71 +9,70 @@ def run_optimization(var, SWcoor, Misc, eps_geom=1, save_folder="Optimization_Lo
     x0 = np.array(var, dtype=float)
 
     bounds = [
-        (30.0, 60.0),  # R0
-        (31.0, 70.0),  # R1
-        (8.0,  20.0),  # R2
-        (9.0,  25.0)   # R3
+        (10.0, 60.0),  # R0
+        (11.0, 70.0),  # R1
+        (1.0,  20.0),  # R2
+        (2.0,  25.0)   # R3
     ]
 
-    def constraint_values(x):             # This is the function that is used to calculate the constraint values
-        utils = PostProcessor()
-        Util_LB_values = utils.Util_LB(x, Misc)
-        Util_NF_values = utils.Util_NF(x, Misc)
-        Util_S_values = utils.Util_S(x, Misc)
-        Util_T_values = utils.Util_T(x, Misc)
-        Util_BNS_values = utils.Util_BNS(x, Misc)
-        Util_BR_values = utils.Util_BR(x, Misc)
-        Util_IN_values = utils.Util_IN(x, Misc)
+    def constraint_values(x):
         """
         Return all inequality constraints in the form c(x) >= 0.
-        Add as many as you want here.
+
+        To add a new constraint later, just add one more line
+        to the list below.
         """
-        return np.array([
-            x[1] - x[0] - eps_geom,
-            x[3] - x[2] - eps_geom,
-            1.0 - Util_LB_values[0],           # local buckling column
-            1.0 - Util_LB_values[1],           # local buckling brace
-            1.0 - Util_NF_values[0],           # normal force column
-            1.0 - Util_NF_values[1],           # normal force brace
-            1.0 - Util_S_values[0],           # shear column
-            1.0 - Util_S_values[1],           # shear brace
-            1.0 - Util_T_values[0],           # Torsion column
-            1.0 - Util_T_values[1],           # Torsion brace
-            1.0 - Util_BNS_values[0],           # bending, normal and shear column
-            1.0 - Util_BNS_values[1],           # bending, normal and shear brace
-            1.0 - Util_BR_values[0],           # Flexural and torsional buckling brace
-            1.0 - Util_BR_values[1],           # Flexural and torsional buckling brace
-            1.0 - Util_IN_values[0],           # Interaction column
-            1.0 - Util_IN_values[1],           # Interaction brace
-            # add more constraints here later if needed
-        ], dtype=float)
+        utils = PostProcessor()
 
-    constraint_names = [
-        "thickness_col",
-        "thickness_brace",
-        "local_buckling_column",
-        "local_buckling_brace",
-        "normal_force_column",
-        "normal_force_brace",
-        "shear_column",
-        "shear_brace",
-        "torsion_column",
-        "torsion_brace",
-        "bending_normal_shear_column",  # Bending, Normal and Shear column
-        "bending_normal_shear_brace",
-        "flexural_torsional_buckling_column",  # Flexural and Torsional buckling column
-        "flexural_torsional_buckling_brace",
-        "interaction_column",  # Interaction column
-        "interaction_brace",  # Interaction brace
-        # add more names here if you add more constraints
-    ]
+        Util_LB_values  = utils.Util_LB(x, Misc)
+        Util_NF_values  = utils.Util_NF(x, Misc)
+        Util_S_values   = utils.Util_S(x, Misc)
+        Util_T_values   = utils.Util_T(x, Misc)
+        Util_BNS_values = utils.Util_BNS(x, Misc)
+        Util_BR_values  = utils.Util_BR(x, Misc)
+        Util_IN_values  = utils.Util_IN(x, Misc)
 
-    constraints = [                 # This is the constraints list that is used to set the constraints for the optimization process
+        constraint_list = [
+            lambda: x[1] - x[0] - eps_geom,      # thickness column
+            lambda: x[3] - x[2] - eps_geom,      # thickness brace
+
+            lambda: 1.0 - Util_LB_values[0],     # local buckling column
+            lambda: 1.0 - Util_LB_values[1],     # local buckling brace
+
+            lambda: 1.0 - Util_NF_values[0],     # normal force column
+            lambda: 1.0 - Util_NF_values[1],     # normal force brace
+
+            lambda: 1.0 - Util_S_values[0],      # shear column
+            lambda: 1.0 - Util_S_values[1],      # shear brace
+
+            lambda: 1.0 - Util_T_values[0],      # torsion column
+            lambda: 1.0 - Util_T_values[1],      # torsion brace
+
+            lambda: 1.0 - Util_BNS_values[0],    # bending, normal and shear column
+            lambda: 1.0 - Util_BNS_values[1],    # bending, normal and shear brace
+
+            lambda: 1.0 - Util_BR_values[0],     # flexural / torsional buckling column
+            lambda: 1.0 - Util_BR_values[1],     # flexural / torsional buckling brace
+
+            lambda: 1.0 - Util_IN_values[0],     # interaction column
+            lambda: 1.0 - Util_IN_values[1],     # interaction brace
+
+            # Add future constraints here, example:
+            # lambda: 3.0 - some_displacement_value,
+            # lambda: 1.0 - utils.Util_SOMETHING(x, Misc)[0],
+            # lambda: 1.0 - utils.Util_SOMETHING(x, Misc)[1],
+        ]
+
+        return np.array([c() for c in constraint_list], dtype=float)
+
+    n_constraints = len(constraint_values(x0))
+
+    constraints = [
         {"type": "ineq", "fun": lambda x, i=i: constraint_values(x)[i]}
-        for i in range(len(constraint_names)) 
+        for i in range(n_constraints)
     ]
 
-    options = {                   # This is the options dictionary that is used to set the optimization options
+    options = {
         "disp": True,
         "eps": 0.1,
         "ftol": 1e-3,
@@ -89,15 +87,15 @@ def run_optimization(var, SWcoor, Misc, eps_geom=1, save_folder="Optimization_Lo
         save_folder=save_folder,
     )
 
-    def objective(x):                # This is the objective function that is used to optimize the design variables
+    def objective(x):
         val = RunAPDL(SWcoor, x, Misc)
         logger.log_evaluation(x, val)
         return val
 
-    def callback(xk):               # This is the callback function that is called after each iteration in the optimization process
+    def callback(xk):
         logger.log_iteration(xk)
 
-    result = spo.minimize(               # This is the optimization function that is used to optimize the design variables
+    result = spo.minimize(
         objective,
         x0,
         method="SLSQP",
