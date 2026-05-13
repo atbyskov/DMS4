75a056ffe4b842365cf2b3a89945592b8ad7b959
Andreas Byskov <atbyskov@gmail.com>
2026-05-13 12:13:23 +0200

    commit

--- 05_OPTI/Main_Test.py
+++ 05_OPTI/Main_Test.py
@@ -36,8 +36,8 @@ opti_settings = {
 # Initial Guess
 column_diameter = 48.3 # Column Diameter [mm]
 column_thickness = 2.5 # Column Thickness [mm]
-brace_diameter = 25  # Brace Diameter [mm]
-brace_thickness = 2  # Brace Thickness [mm]
+brace_diameter = 24.0  # Brace Diameter [mm]
+brace_thickness = 1.0  # Brace Thickness [mm]
 
 # Bounds
 column_diameter_bounds = (50.0, 100)   # Column Diameter Bounds [mm]
@@ -47,7 +47,7 @@ brace_thickness_bounds = (1.0, 4.5)    # Brace Thickness Bounds [mm]
 
 # Defining variables with bounds and active status
 var = {
-    "rad": {"value": 337.46, "bounds": (150.0, 300.0), "active": True}, # Radius Structure [mm]
+    "rad": {"value": 291.54, "bounds": (150.0, 300.0), "active": True}, # Radius Structure [mm]
 }
 if opti_settings["multi_size_columns"]:
     var.update({
@@ -92,7 +92,7 @@ Misc = {
     "Hor_Force": 502.52,                 # Horizontal Force (P_Load_z)       [N]
     "Ver_Force": -25.13E+3,              # Vertical Force (P_Load_y)         [N]
     "f_y": 700 ,                         # Column Yield Strength             [MPa]
-    "f_y_brace": 355,                    # Brace Yield Strength              [MPa]
+    "f_y_brace": 235,                    # Brace Yield Strength              [MPa]
     "E_mod": 200*1E3,                    # Youngs Modulus                    [MPa]
     "W_Force": -3.751E+3,                # Vertical Force COG (P_COG_y)      [N]
     "SW_filename": "LWC_L1_LINES.IGS"    # Filename for IGS File
