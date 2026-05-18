755d8bcb7ae1dbcf9ab96b2562998a83b508a6b9
Zeetox12345 <christiandam77@gmail.com>
2026-05-18 08:30:44 +0200

    Update optimization settings: enable multi-size columns and braces, adjust brace split, and modify diameter and thickness bounds for columns and braces.

--- 05_OPTI/Main_Self_Written_Test.py
+++ 05_OPTI/Main_Self_Written_Test.py
@@ -17,9 +17,9 @@ opti_settings = {
     "n_mast_segments": 5,         # Number of mast segments
     "mast_segment_height": 810,   # Height of each mast segment [mm]
     "segment_mass_limit": 23,     # Limits for segment masses [kg]
-    "multi_size_columns": False,   # Whether mast segments columns uses different dimensions (True) or not (False)
-    "multi_size_braces": False,    # Whether mast segments braces uses different dimensions (True) or not (False)
-    "brace_split": False,          # Whether braces are split between horiontal and cross (True) or not (False)
+    "multi_size_columns": True,   # Whether mast segments columns uses different dimensions (True) or not (False)
+    "multi_size_braces": True,    # Whether mast segments braces uses different dimensions (True) or not (False)
+    "brace_split": True,          # Whether braces are split between horiontal and cross (True) or not (False)
 }
 
 # Initial Guess
@@ -29,10 +29,10 @@ brace_diameter = 26.9 # Brace Diameter [mm]
 brace_thickness = 2.3 # Brace Thickness [mm]
 
 # Bounds
-column_diameter_bounds = (40.0, 100) # Column Diameter Bounds [mm]
-column_thickness_bounds = (1.0, 7.0)   # Column Thickness Bounds [mm]
-brace_diameter_bounds = (10.0, 100.0)   # Brace Diameter Bounds [mm]
-brace_thickness_bounds = (1.0, 4.5)    # Brace Thickness Bounds [mm]
+column_diameter_bounds = (48.3, 114.3) # Column Diameter Bounds [mm]
+column_thickness_bounds = (2.5, 5.0)   # Column Thickness Bounds [mm]
+brace_diameter_bounds = (10.0, 50.0)   # Brace Diameter Bounds [mm]
+brace_thickness_bounds = (1.0, 4.0)    # Brace Thickness Bounds [mm]
 
 # Defining variables with bounds and active status
 var = {
